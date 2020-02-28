efb5eb72bd6df8a8f1823921410bd84485b03ac1
Michael Carroll <carrollm2@gmail.com>
2020-02-28 03:09:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,8 +11,8 @@ class App < Sinatra::Base
     @coach = params["coach"]
     @pg = params["pg"]
     @sg = params["sg"]
-    @small_forward = params["sf"]
-    @power_forward = params["pf"]
+    @sf = params["sf"]
+    @pf = params["pf"]
     @center = params["c"]
     erb :team
   end
